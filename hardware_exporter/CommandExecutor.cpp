bd35d28d5edd0db063f449deba0ba8f8b062fd61
Vladislav-Marinkin <45194111+Vladislav-Marinkin@users.noreply.github.com>
2025-03-31 10:50:52 +0400

    commit

--- hardware_exporter/CommandExecutor.cpp
+++ hardware_exporter/CommandExecutor.cpp
@@ -7,32 +7,32 @@ std::string CommandExecutor::runCommand(const std::string& command)
     std::string result;
 
     // Открываем поток для выполнения команды
-    /*FILE* pipe = popen(command.c_str(), "r");
+    FILE* pipe = popen(command.c_str(), "r");
     if (!pipe) {
         throw std::runtime_error("Error executing the command");
-    }*/
+    }
 
     // Читаем вывод команды и добавляем его в результат
-    /*while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
+    while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
         result += buffer.data();
-    }*/
+    }
 
     // Читаем ошибки из stderr, если они есть
-    /*FILE* errorPipe = popen((command + " 2>&1").c_str(), "r");
+    FILE* errorPipe = popen((command + " 2>&1").c_str(), "r");
     if (errorPipe) {
         while (fgets(buffer.data(), buffer.size(), errorPipe) != nullptr) {
             errorOutput += buffer.data();
         }
         fclose(errorPipe);
-    }*/
+    }
 
     // Если в stderr есть ошибки, выбрасываем исключение
-    /*if (!errorOutput.empty()) {
+    if (!errorOutput.empty()) {
         throw std::runtime_error("Error executing the command: " + errorOutput);
-    }*/
+    }
 
     // Закрываем пайп
-    //fclose(pipe);
+    fclose(pipe);
 
     return result;
 }
